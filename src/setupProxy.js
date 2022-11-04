6aa5dc71429b1d175161c796b74af133ced2251c
David <dmatimu@udel.edu>
2022-11-04 01:27:21 -0400

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,8 +2,8 @@ const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
 
-    app.use(proxy('/api', { target: 'http://10.96.122.236:8080/' }));
-    app.use(proxy('/account', { target: 'http://10.111.248.128:8081/' })); 
+    app.use(proxy('/api', { target: 'http://10.99.1.194:8080/' }));
+    app.use(proxy('/account', { target: 'http://10.102.87.204:8081/' })); 
 	
 	/*app.use(proxy('/api', { target: 'http://172.18.0.2:8080/' }));
 	app.use(proxy('/account', { target: 'http://172.18.0.3:8081/' }));
